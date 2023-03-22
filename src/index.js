6de198cf44d8cbe7ed01bbe73caa9ecec63e330a
nxn_404 <nafeurrahmanadil@gmail.com>
2023-03-22 14:47:00 +0600

    Add list items in the homepage

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 import './style.css';
-import comments from './comments';
 
 const main = document.getElementById('main');
-let movies;
-(async () => {
-  await fetch('https://api.tvmaze.com/shows')
-    .then((response) => response.json())
-    .then((data) => {
-      movies = data;
-      main.innerHTML = movies
-        .map(
-          (movie) => `
-        <article>
-        <img src="${movie.image.medium}">
-        <h1 class="movie-title">${movie.name}</h1>
-        <i class="like-icon">like</i>
-        <button class="comments-btn">comments</button>
-        </article>
-      `
-        )
-        .join('');
-      document.querySelectorAll('.comments').forEach((btn) => {
-        btn.addEventListener('click', () => {
-          comments();
-        });
-      });
+// Fetch data from TVMaze API
+fetch('https://api.tvmaze.com/shows?page=0')
+  .then((response) => response.json())
+  .then((data) => {
+    // Display first 9 shows
+    data.slice(0, 12).forEach((show) => {
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      const name = document.createElement('strong');
+      const commentBtn = document.createElement('button');
+      const likeBtn = document.createElement('span');
+      const footer = document.createElement('div');
+
+      li.className = 'main__list';
+      footer.className = 'main__list__btn';
+
+      img.src = show.image.medium;
+      name.innerText = show.name;
+      commentBtn.innerText = 'Comment';
+      likeBtn.className = 'material-symbols-outlined';
+      likeBtn.innerText = 'favorite';
+
+      li.appendChild(img);
+      li.appendChild(name);
+      footer.appendChild(likeBtn);
+      footer.appendChild(commentBtn);
+      li.appendChild(footer);
+
+      main.appendChild(li);
     });
-})();
+  });
