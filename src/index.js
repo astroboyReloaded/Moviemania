a5d6550ea26f1c4f5f28a54ef8e3e88098d6af9c
nxn_404 <nafeurrahmanadil@gmail.com>
2023-03-22 16:47:21 +0600

    Add like num count

--- src/index.js
+++ src/index.js
@@ -1,29 +1,73 @@
+// Import CSS file
 import './style.css';
-import comments from './comments';
 
+// Get reference to main list element
 const main = document.getElementById('main');
-let movies;
-(async () => {
-  await fetch('https://api.tvmaze.com/shows')
-    .then((response) => response.json())
-    .then((data) => {
-      movies = data;
-      main.innerHTML = movies
-        .map(
-          (movie) => `
-        <article>
-        <img src="${movie.image.medium}">
-        <h1 class="movie-title">${movie.name}</h1>
-        <i class="like-icon">like</i>
-        <button class="comments-btn">comments</button>
-        </article>
-      `
-        )
-        .join('');
-      document.querySelectorAll('.comments').forEach((btn) => {
-        btn.addEventListener('click', () => {
-          comments();
-        });
+
+// Async function to get likes for a show
+async function getLikes(showId) {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8ae878c40e24371ea51406311a0a2161/likes/?item_id=${showId}`);
+  const data = await response.json();
+  return data[0]?.likes || 0;
+}
+
+// Async function to send like count for a show
+async function sendLike(showId, likeCount) {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8ae878c40e24371ea51406311a0a2161/likes/${showId}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ likes: likeCount }),
+  });
+  return response.json();
+}
+
+// Fetch data from TVMaze API
+fetch('https://api.tvmaze.com/shows?page=0')
+  .then(response => response.json())
+  .then(async data => {
+    // Get the first 10 shows
+    const shows = data.slice(0, 10);
+
+    // Get likes for all shows in parallel
+    const showLikes = await Promise.all(shows.map(show => getLikes(show.id)));
+
+    // Loop through all shows
+    shows.forEach((show, i) => {
+      // Create elements for the show
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      const name = document.createElement('strong');
+      const commentBtn = document.createElement('button');
+      const likeBtn = document.createElement('span');
+      const likeCount = document.createElement('span');
+      const footer = document.createElement('div');
+
+      // Set classes for elements
+      li.className = 'main__list';
+      footer.className = 'main__list__btn';
+
+      // Set image source and name text
+      img.src = show.image.medium;
+      name.innerText = show.name;
+      commentBtn.innerText = 'Comment';
+
+      // Set text and classes for like button and count
+      likeBtn.className = 'material-symbols-outlined';
+      likeBtn.innerText = 'favorite';
+      show.likes = showLikes[i];
+      likeCount.innerHTML = `${show.likes} likes`;
+
+      // Add event listener for like button click
+      likeBtn.addEventListener('click', async () => {
+        show.likes++;
+        likeCount.innerHTML = `${show.likes} likes`;
+        const response = await sendLike(show.id, show.likes);
+        console.log(response);
       });
+
+      // Append elements to list item and list item to main list
+      li.append(img, name, footer);
+      footer.append(likeBtn, likeCount, commentBtn);
+      main.appendChild(li);
     });
-})();
+  });
