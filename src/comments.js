b74350b20d20a743eba2490d6c9407b7b0e4f8a1
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-23 21:58:53 -0600

    fix typo commnts.js line 25

--- src/comments.js
+++ src/comments.js
@@ -2,7 +2,7 @@ const getComments = async (id) => {
   const commentsContainer = document.getElementById('commentsSection');
 
   await fetch(
-    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SzueLQl3FaQn1wJjDbsx/comments?item_id=${id}`,
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SzueLQl3FaQn1wJjDbsx/comments?item_id=${id}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -11,18 +11,18 @@ const getComments = async (id) => {
       commentsContainer.innerHTML = `<h2>Comments(${comments.length})</h2>
         <ul class="coments-ul">
           ${comments
-    .map(
-      (each) => `<li class="comment-li">
+            .map(
+              (each) => `<li class="comment-li">
                 <time>${each.creation_date}</time>
                 ${each.username}: ${each.comment}
-                </li>`,
-    )
-    .join('')}
+                </li>`
+            )
+            .join('')}
         </ul>`;
     })
     .catch(() => {
       commentsContainer.innerHTML = `
-    <p>Be the firs to comment!</p>
+    <p>Be the first to comment!</p>
     `;
     });
 };
@@ -38,7 +38,7 @@ const submitComment = async (id, userName, Comment) => {
         username: userName.value,
         comment: Comment.value,
       }),
-    },
+    }
   );
   Comment.value = '';
   Comment.focus();
