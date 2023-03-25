2f6e8db406d8def400a2636ad29318f5b8b0bc56
nxn_404 <nafeurrahmanadil@gmail.com>
2023-03-25 16:20:26 +0600

    Fix according the reviewer

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,23 +1,25 @@
+const renderLikes = (likes) => {
+  const likesContainers = document.querySelectorAll('.likesContainer');
+  likesContainers.forEach((container) => {
+    likes.filter((like) => {
+      if (Number(container.id) === like.item_id) {
+        container.innerText = like.likes;
+        return true;
+      }
+      return false;
+    });
+  });
+};
+
 // Async function to get likes for a show
 export const getLikes = async () => {
   await fetch(
     'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SzueLQl3FaQn1wJjDbsx/likes',
   )
     .then((response) => response.json())
-    // eslint-disable-next-line no-use-before-define
     .then((data) => renderLikes(data));
 };
-const renderLikes = (likes) => {
-  const likesContainers = document.querySelectorAll('.likesContainer');
-  likesContainers.forEach((container) => {
-    // eslint-disable-next-line array-callback-return
-    likes.filter((like) => {
-      if (Number(container.id) === like.item_id) {
-        container.innerText = like.likes;
-      }
-    });
-  });
-};
+
 // Async function to send like count for a show
 export const sendLike = async (id) => {
   await fetch(
